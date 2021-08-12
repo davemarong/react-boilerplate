7019d0451096d9d8f007a7affcf9389d9c3d9705
davemarong <kmarong@hotmail.com>
2021-08-12 12:26:15 +0200

    Completed node script - delete extra files

--- fixBoilerplate.js
+++ fixBoilerplate.js
@@ -1,13 +1,48 @@
 const fs = require("fs");
-const path = require("path");
 
-console.log(__dirname);
+const publicFilesToDelete = [];
+const srcFilesToDelete = [];
 
-const publicFiles = fs.readdirSync("./public");
-const srcFiles = fs.readdirSync("./src");
+fs.readdir("./public", (err, files) => {
+  if (err) {
+    throw err;
+  }
+  publicFilesToDelete.push(...files);
 
-console.log(publicFiles, srcFiles);
+  for (let i = 0; i < publicFilesToDelete.length; i++) {
+    if (publicFilesToDelete[i] === `index.html`) {
+      publicFilesToDelete.splice(i, 1);
+    }
+  }
+  deletePublicFiles("public", publicFilesToDelete);
+  process.stdout.write(
+    `\n \n \n You have deleted these files from the folder "public": \n ${publicFilesToDelete}`
+  );
+});
+fs.readdir("./src", (err, files) => {
+  if (err) {
+    throw err;
+  }
+  srcFilesToDelete.push(...files);
+  let number = srcFilesToDelete.length;
+  let i;
+  for (i = 0; i < number; i++) {
+    if (
+      srcFilesToDelete[i] === "App.js" ||
+      srcFilesToDelete[i] === "index.js"
+    ) {
+      srcFilesToDelete.splice(i, 1);
+      i = i - 1;
+    }
+  }
+  deletePublicFiles("src", srcFilesToDelete);
+  process.stdout.write(
+    `\n \n \n You have deleted these files from the folder "src": \n ${srcFilesToDelete} \n \n \n`
+  );
+});
 
-const removePublicFiles = (file) => {
-  fs.unlinkSync(`./public/${file}`);
+const deletePublicFiles = (folder, fileArray) => {
+  for (let i = 0; i < fileArray.length; i++) {
+    fs.unlinkSync(`./${folder}/${fileArray[i]}`);
+  }
 };
